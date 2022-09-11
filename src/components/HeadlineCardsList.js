aa5bfbb9e4e0cbb3b47146d6a43aaa30a5873458
yash <yashgouravkar@gmail.com>
2022-09-11 17:18:24 +0530

    broke HeadlineCards into 3 reusable components

--- src/components/HeadlineCardsList.js
+++ src/components/HeadlineCardsList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HeadlineCard from "./HeadlineCard";
 
-const HeadlineCards = () => {
+const HeadlineCardsList = () => {
   return (
     <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6 ">
       <HeadlineCard
@@ -28,4 +28,4 @@ const HeadlineCards = () => {
   );
 };
 
-export default HeadlineCards;
+export default HeadlineCardsList;
